634278fa7392fc22c146db02a4067d0b269976dc
Hukuta <nikitachum99@gmail.com>
2023-03-22 13:03:44 +0300

    description of changes

--- src/main/java/com/example/sweater/service/UserService.java
+++ src/main/java/com/example/sweater/service/UserService.java
@@ -9,8 +9,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
-import java.util.UUID;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService implements UserDetailsService {
@@ -25,30 +25,35 @@ public class UserService implements UserDetailsService {
         return userRepo.findByUsername(username);
     }
 
-    public boolean addUser(User user){
+    public boolean addUser(User user) {
         User userFromDb = userRepo.findByUsername(user.getUsername());
 
         if (userFromDb != null) {
             return false;
         }
 
-        user.setActive(true);
+        user.setActive(false);
         user.setRoles(Collections.singleton(Role.USER));
         user.setActivationCode(UUID.randomUUID().toString());
 
         userRepo.save(user);
 
-        if (user.getEmail()!=null && !user.getEmail().trim().isEmpty()){
+        sendMessage(user);
+
+        return true;
+    }
+
+    private void sendMessage(User user) {
+        if (user.getEmail() != null && !user.getEmail().trim().isEmpty()) {
             String message = String.format(
                     "Hello, %s! \n" +
-                            "Welcome to the Sweater. Please, visit link: http://localhost:8080/activate/%s",
+                            "Welcome to the Sweater! Please, activate your profile: http://localhost:8080/activate/%s",
                     user.getUsername(),
                     user.getActivationCode()
             );
+
             mailSenderService.send(user.getEmail(), "Activation code", message);
         }
-
-        return true;
     }
 
     public boolean activateUser(String code) {
@@ -58,10 +63,59 @@ public class UserService implements UserDetailsService {
             return false;
         }
 
+        user.setActive(true);
+
         user.setActivationCode(null);
 
         userRepo.save(user);
 
         return true;
+    }
+
+    public List<User> findAll() {
+        return userRepo.findAll();
+    }
+
+    public void saveUser(User user, String username, Map<String, String> form) {
+        user.setUsername(username);
+
+        Set<String> roles = Arrays.stream(Role.values())
+                .map(Role::name)
+                .collect(Collectors.toSet());
+
+        user.getRoles().clear();
+
+        for (String key : form.keySet()) {
+            if (roles.contains(key)) {
+                user.getRoles().add(Role.valueOf(key));
+            }
+        }
+
+        userRepo.save(user);
+    }
+
+    public void updateProfile(User user, String password, String email) {
+        String userEmail = user.getEmail();
+
+        boolean isEmailChanged = (email != null && !email.equals(userEmail)) ||
+                (userEmail != null && !userEmail.equals(email));
+
+        if (isEmailChanged) {
+            user.setEmail(email);
+
+            if (email != null && !email.trim().isEmpty()) {
+                user.setActivationCode(UUID.randomUUID().toString());
+            }
+
+            if (password != null && !password.trim().isEmpty()) {
+                user.setPassword(password);
+            }
+
+            userRepo.save(user);
+
+            if (isEmailChanged){
+                sendMessage(user);
+            }
+        }
     }
 }
