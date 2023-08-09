bc896162b59a1242b873f594d8c789281edbbecb
Hukutach <nikitachum99@gmail.com>
2023-08-09 11:59:59 +0300

    some changes

--- src/main/java/com/example/sweater/domain/User.java
+++ src/main/java/com/example/sweater/domain/User.java
@@ -10,6 +10,7 @@ import java.util.Set;
 @Entity
 @Table(name = "usr")
 public class User implements UserDetails {
+    //some changes
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
